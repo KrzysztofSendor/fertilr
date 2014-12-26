ab879c415480ff56ae3da8cf640119a785b3ed08
Krzysztof Sendor <sendor.krzysztof@gmail.com>
2014-12-27 00:29:20 +0100

    Code cleaning

--- index.php
+++ index.php
@@ -25,14 +25,13 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-md-4 col-md-offset-4 col-sm-10 col-sm-offset-1">
-				<?php
-	if (!isset($_SESSION['auth'])) $_SESSION['auth'] = false;
-	
-	if (!isset($_POST['login']) && !isset($_POST['password']) && $_SESSION['auth'] == FALSE) {
-	?>
 				<div class="panel panel-default">
 					<div class="panel-heading">Logowanie</div>
 					<div class="panel-body">
+					<?php
+					if (!isset($_SESSION['auth'])) $_SESSION['auth'] = false;
+					if (!isset($_POST['login']) && !isset($_POST['password']) && $_SESSION['auth'] == FALSE) {
+					?>
 						<form name="form-logowanie" action="index.php" method="post">
 							<div class="form-group">
 								<label for="LoginInput">Login</label>
@@ -44,79 +43,62 @@
 							</div>
 							<button type="submit" class="btn btn-default" name="zaloguj">Zaloguj</button>
 						</form>
-					</div>
-				</div>
-				<?php
-	} 
-	elseif (isset($_POST['login']) && isset($_POST['password']) && $_SESSION['auth'] == FALSE) {
-		if(!empty($_POST['login']) && !empty($_POST['password'])) {
-			$login = mysql_real_escape_string($_POST['login']);
-			$password = mysql_real_escape_string($_POST['password']);
-			$password = md5($password);
-			
-			$sql = mysql_num_rows(mysql_query("SELECT login, haslo FROM `Uzytkownicy` WHERE `login` = '$login' AND `haslo` = '$password'"));
-			
-			if ($sql == 1) {
-				$_SESSION['user'] = $login;
-				$_SESSION['auth'] = TRUE;
-				
-				$sql = mysql_num_rows(mysql_query("SELECT login, haslo, admin FROM `Uzytkownicy` WHERE `login` = '$login' AND `haslo` = '$password' AND `admin` = 1"));
-			
-				if ($sql == 1){
-					$_SESSION['admin'] = TRUE;
-					header("Location: admin.php");
-				} else {
-					$_SESSION['admin'] = FALSE;
-					header("Location: user.php");
-				}
-			} else {
-			?>
-				<div class="panel panel-default">
-					<div class="panel-heading">Logowanie</div>
-					<div class="panel-body">
-						<div class="alert alert-danger" role="alert">Błędne dane logowania</div>
-						<form action="index.php">
-							<button type="submit" class="btn btn-default">Wróć do logowania</button>
-						</form>
-					</div>
-				</div>
-				<?php
-			}
-		} else {
-		?>
-			<div class="panel panel-default">
-				<div class="panel-heading">Logowanie</div>
-				<div class="panel-body">
+					<?php
+					} 
+					elseif (isset($_POST['login']) && isset($_POST['password']) && $_SESSION['auth'] == FALSE) {
+						if(!empty($_POST['login']) && !empty($_POST['password'])) {
+							$login = mysql_real_escape_string($_POST['login']);
+							$password = mysql_real_escape_string($_POST['password']);
+							$password = md5($password);
+							
+							$sql = mysql_num_rows(mysql_query("SELECT login, haslo FROM `Uzytkownicy` WHERE `login` = '$login' AND `haslo` = '$password'"));
+							
+							if ($sql == 1) {
+								$_SESSION['user'] = $login;
+								$_SESSION['auth'] = TRUE;
+								
+								$sql = mysql_num_rows(mysql_query("SELECT login, haslo, admin FROM `Uzytkownicy` WHERE `login` = '$login' AND `haslo` = '$password' AND `admin` = 1"));
+							
+								if ($sql == 1){
+									$_SESSION['admin'] = TRUE;
+									header("Location: admin.php");
+								} else {
+									$_SESSION['admin'] = FALSE;
+									header("Location: user.php");
+								}
+							} else {
+					?>
+					<div class="alert alert-danger" role="alert">Błędne dane logowania</div>
+					<form action="index.php">
+						<button type="submit" class="btn btn-default">Wróć do logowania</button>
+					</form>
+					<?php
+							}
+						} else {
+					?>
 					<div class="alert alert-danger" role="alert">Brak danych logowania</div>
 					<form action="index.php">
 						<button type="submit" class="btn btn-default">Wróć do logowania</button>
 					</form>
+					<?php
+						}
+					}
+					elseif ($_SESSION['auth'] == TRUE && isset($_GET['logout'])){
+						$_SESSION['user'] = FALSE;
+						$_SESSION['auth'] = '';
+					?>
+					<div class="alert alert-success" role="alert">Wylogowano</div>
+					<form action="index.php">
+						<button type="submit" class="btn btn-default">Wróć do logowania</button>
+					</form>
+					<?php
+					}
+					elseif ($_SESSION['auth'] == TRUE && $_SESSION['admin'] == TRUE) header("Location: admin.php");
+					elseif ($_SESSION['auth'] == TRUE && $_SESSION['admin'] == FALSE) header("Location: user.php");
+					mysql_close();
+					?>
+					</div>
 				</div>
-			</div>
-			<?php
-		}
-	}
-	elseif ($_SESSION['auth'] == TRUE && isset($_GET['logout'])){
-		$_SESSION['user'] = FALSE;
-		$_SESSION['auth'] = '';
-		?>
-		<div class="panel panel-default">
-			<div class="panel-heading">Logowanie</div>
-			<div class="panel-body">
-				<div class="alert alert-success" role="alert">Wylogowano</div>
-				<form action="index.php">
-					<button type="submit" class="btn btn-default">Wróć do logowania</button>
-				</form>
-			</div>
-		</div>
-		<?php
-	}
-	
-	elseif ($_SESSION['auth'] == TRUE && $_SESSION['admin'] == TRUE) header("Location: admin.php");
-	elseif ($_SESSION['auth'] == TRUE && $_SESSION['admin'] == FALSE) header("Location: user.php");
-
-	mysql_close();
-	?>
 			</div>
 		</div>
 	</div>
