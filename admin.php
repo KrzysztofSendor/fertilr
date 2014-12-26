9098ca36cb255740d163a71125c42b2efdb4775f
Krzysztof Sendor <sendor.krzysztof@gmail.com>
2014-12-26 23:25:56 +0100

    Added Bootstrap classes
    Added Bootstrap (v3.3.1) classes to admin.php and index.php
    Ignored css, fonts, js folders in .gitignore

--- admin.php
+++ admin.php
@@ -3,28 +3,39 @@
 	session_start();
 	require_once('db.php');
 ?>
-<html>
+<html lang="pl">
 <head>
 	<title>fertil | admin</title>
 	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<link href="css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 	<?php
 		if($_SESSION['auth'] == TRUE && $_SESSION['admin'] == TRUE){
+			$imie = mysql_fetch_array(mysql_query("SELECT imie FROM `Uzytkownicy` WHERE `login` = '".$_SESSION['user']."'"));
+			$imie = $imie[0];
+			$nazwisko = mysql_fetch_array(mysql_query("SELECT nazwisko FROM `Uzytkownicy` WHERE `login` = '".$_SESSION['user']."'"));
+			$nazwisko = $nazwisko[0];
 	?>
-	SUKCES!<br>
+	<div class="jumbotron">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-11 col-md-offset-1 col-sm-11 col-sm-offset-1">	
+					<h1>fertilr</h1>
+					<p>Witaj <?php echo($imie); echo(" "); echo($nazwisko) ?></p>
+				</div>
+			</div>
+		</div>
+	</div>
 	
 	<a href="index.php?logout">Wyloguj się</a>
-	
-	
-	
-	
-	
 	
 	<?php
 		} else {
 			header("Location: index.php");
 		}
+		mysql_close();
 	?>
 </body>
 </html>
--- index.php
+++ index.php
@@ -3,26 +3,50 @@
 	session_start();
 	require_once('db.php');
 ?>
-<html>
+<html lang="pl">
 <head>
 	<title>fertilr</title>
 	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<link href="css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
-	<?php
+	<div class="jumbotron">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-11 col-md-offset-1 col-sm-11 col-sm-offset-1">	
+					<h1>fertilr</h1>
+					<p>Proste narzędzie do przeprowadzania doświadczeń botanicznych</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4 col-md-offset-4 col-sm-10 col-sm-offset-1">
+				<?php
 	if (!isset($_SESSION['auth'])) $_SESSION['auth'] = false;
 	
 	if (!isset($_POST['login']) && !isset($_POST['password']) && $_SESSION['auth'] == FALSE) {
 	?>
-	<form name="form-logowanie" action="index.php" method="post">
-		Login:<br>
-		<input type="text" name="login"><br>
-		Hasło:<br>
-		<input type="password" name="password"><br>
-		<input type="submit" name="zaloguj" value="Zaloguj">
-	</form>
-	<?php
+				<div class="panel panel-default">
+					<div class="panel-heading">Logowanie</div>
+					<div class="panel-body">
+						<form name="form-logowanie" action="index.php" method="post">
+							<div class="form-group">
+								<label for="LoginInput">Login</label>
+								<input type="text" class="form-control" placeholder="Wprowadź login" name="login">
+							</div>
+							<div class="form-group">
+								<label for="PasswordInput">Hasło</label>
+								<input type="password" class="form-control" placeholder="Wprowadź hasło" name="password">
+							</div>
+							<button type="submit" class="btn btn-default" name="zaloguj">Zaloguj</button>
+						</form>
+					</div>
+				</div>
+				<?php
 	} 
 	elseif (isset($_POST['login']) && isset($_POST['password']) && $_SESSION['auth'] == FALSE) {
 		if(!empty($_POST['login']) && !empty($_POST['password'])) {
@@ -40,34 +64,61 @@
 			
 				if ($sql == 1){
 					$_SESSION['admin'] = TRUE;
-/* 					echo '<meta http-equiv="refresh" content"1; URL=admin.php">'; */
 					header("Location: admin.php");
 				} else {
 					$_SESSION['admin'] = FALSE;
-/* 					echo '<meta http-equiv="refresh" content"1; URL=user.php">'; */
 					header("Location: user.php");
 				}
 			} else {
-				echo 'Błąd logowania';
-				echo '<a href="index.php">Wróć do logowania</a>';
+			?>
+				<div class="panel panel-default">
+					<div class="panel-heading">Logowanie</div>
+					<div class="panel-body">
+						<div class="alert alert-danger" role="alert">Błędne dane logowania</div>
+						<form action="index.php">
+							<button type="submit" class="btn btn-default">Wróć do logowania</button>
+						</form>
+					</div>
+				</div>
+				<?php
 			}
 		} else {
-			echo 'Błąd logowania';
-			echo '<a href="index.php">Wróć do logowania</a>';
+		?>
+			<div class="panel panel-default">
+				<div class="panel-heading">Logowanie</div>
+				<div class="panel-body">
+					<div class="alert alert-danger" role="alert">Brak danych logowania</div>
+					<form action="index.php">
+						<button type="submit" class="btn btn-default">Wróć do logowania</button>
+					</form>
+				</div>
+			</div>
+			<?php
 		}
 	}
 	elseif ($_SESSION['auth'] == TRUE && isset($_GET['logout'])){
 		$_SESSION['user'] = FALSE;
 		$_SESSION['auth'] = '';
-		echo 'Wylogowano<br><a href="index.php">Powrót do strony logowania</a>';
+		?>
+		<div class="panel panel-default">
+			<div class="panel-heading">Logowanie</div>
+			<div class="panel-body">
+				<div class="alert alert-success" role="alert">Wylogowano</div>
+				<form action="index.php">
+					<button type="submit" class="btn btn-default">Wróć do logowania</button>
+				</form>
+			</div>
+		</div>
+		<?php
 	}
 	
 	elseif ($_SESSION['auth'] == TRUE && $_SESSION['admin'] == TRUE) header("Location: admin.php");
 	elseif ($_SESSION['auth'] == TRUE && $_SESSION['admin'] == FALSE) header("Location: user.php");
 
-	
-	
+	mysql_close();
 	?>
-	
+			</div>
+		</div>
+	</div>
 </body>
 </html>
