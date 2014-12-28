6ea86c4cb30d7b5ff4a4de462a235241d11b13e5
Krzysztof Sendor <sendor.krzysztof@gmail.com>
2014-12-28 21:16:01 +0100

    Front-end changes

--- admin.php
+++ admin.php
@@ -35,19 +35,23 @@
 	</div>
 	<div class="container">
 		<div class="row">
-			<div class="col-md-5 col-md-offset-1 col-sm-10 col-sm-offset-1">
-				<div class="well well-lg">
-					 <button type="submit" class="btn btn-default btn-lg btn-block" onclick="location.href='admin/users.php'">Użytkownicy</button>
-					 <button type="submit" class="btn btn-default btn-lg btn-block" onclick="location.href='#'">Rośliny</button>
-					 <button type="submit" class="btn btn-default btn-lg btn-block" onclick="location.href='#'">Rośliny</button>
-					 <button type="submit" class="btn btn-default btn-lg btn-block" onclick="location.href='#'">Rośliny</button>
-					 <button type="submit" class="btn btn-default btn-lg btn-block" onclick="location.href='#'">Rośliny</button>
-					 <button type="submit" class="btn btn-default btn-lg btn-block" onclick="location.href='#'">Rośliny</button>
+			<div class="col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1">
+				<div class="row">
+					<div class="col-md-6 col-sm-12">
+						<div class="well well-lg">
+							 <button type="submit" class="btn btn-default btn-lg btn-block" onclick="location.href='admin/users.php'">Użytkownicy</button>
+							 <button type="submit" class="btn btn-default btn-lg btn-block" onclick="location.href='#'">Rośliny</button>
+							 <button type="submit" class="btn btn-default btn-lg btn-block" onclick="location.href='#'">Rośliny</button>
+							 <button type="submit" class="btn btn-default btn-lg btn-block" onclick="location.href='#'">Rośliny</button>
+							 <button type="submit" class="btn btn-default btn-lg btn-block" onclick="location.href='#'">Rośliny</button>
+							 <button type="submit" class="btn btn-default btn-lg btn-block" onclick="location.href='#'">Rośliny</button>
+						</div>
+					</div>
+					<div class="col-md-6 col-sm-12">
+						<h2>Statystyki</h2>
+						<p>Tu pojawią się statystyki</p>
+					</div>
 				</div>
-			</div>
-			<div class="col-md-5 col-sm-10 col-sm-offset-1">
-				<h2>Statystyki</h2>
-				<p>Tu pojawią się statystyki</p>
 			</div>
 		</div>
 	</div>
